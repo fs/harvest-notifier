63e3c7976219a19536125fccde534f84d06e5683
Vadim-Kurnatovskiy <vadim.kurnatovskiy@flatstack.com>
2020-04-17 18:55:01 +0300

    quality fix

--- Rakefile
+++ Rakefile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 lib_dir = File.expand_path("lib", File.dirname(__FILE__))
 $LOAD_PATH << lib_dir unless $LOAD_PATH.include?(lib_dir)
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+require "timecop"
+require "byebug"
+require "dotenv"
+
+Dotenv.load(".env.example")
+
+module Helpers
+  def fixture(name)
+    JSON.parse(File.read("spec/fixtures/#{name}.json"))
+  end
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.syntax = :expect

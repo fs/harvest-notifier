19dbf46b27e8227ae1f053a6dfadc1be5699c3f9
Timur Vafin <timur.vafin@flatstack.com>
2020-04-30 16:25:50 +0300

    Cleanup harvest_notifier_spec

--- spec/harvest_notifier_spec.rb
+++ spec/harvest_notifier_spec.rb
@@ -4,36 +4,22 @@ describe HarvestNotifier do
   let(:base_bouble) { instance_double(HarvestNotifier::Base) }
 
   before do
-    allow(HarvestNotifier::Base)
-      .to receive(:new) { base_bouble }
+    allow(HarvestNotifier::Base).to receive(:new) { base_bouble }
+
+    allow(base_bouble).to receive(:create_daily_report)
+    allow(base_bouble).to receive(:create_weekly_report)
   end
 
   describe "#create_daily_report" do
-    before do
-      allow(base_bouble).to receive(:create_daily_report)
-    end
-
     it "creates daily report" do
-      expect(base_bouble)
-        .to receive(:create_daily_report)
-      expect(base_bouble)
-        .not_to receive(:create_weekly_report)
-
+      expect(base_bouble).to receive(:create_daily_report)
       described_class.create_daily_report
     end
   end
 
   describe "#create_weekly_report" do
-    before do
-      allow(base_bouble).to receive(:create_weekly_report)
-    end
-
     it "creates daily report" do
-      expect(base_bouble)
-        .to receive(:create_weekly_report)
-      expect(base_bouble)
-        .not_to receive(:create_daily_report)
-
+      expect(base_bouble).to receive(:create_weekly_report)
       described_class.create_weekly_report
     end
   end

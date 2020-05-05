47885ca117908fc890356e5e4ed9970cb052ce4c
Vadim Kurnatovskiy <49876756+Vadim-Kurnatovskiy@users.noreply.github.com>
2020-05-05 17:26:03 +0300

    Add weekly report (#7)
    * Add weekly report
    
    * rspec and quality
    
    * env fix
    
    * add var to env example
    
    * Refacor report
    
    * my proposed changes
    
    * Revert "my proposed changes"
    
    This reverts commit 36db7edd0612bd10546ea02659c2e90db961a226.
    
    * return only values
    
    * Remove unused deep_merge
    
    * Hide users reject in filter_users_with_reports
    
    * Rename whitelisted_email? to whitelisted_user?
    
    * Reorder methods
    
    Co-authored-by: Timur Vafin <timur.vafin@flatstack.com>

--- lib/harvest-notifier/report.rb
+++ lib/harvest-notifier/report.rb
@@ -4,44 +4,83 @@ require "active_support/core_ext/module/delegation"
 
 module HarvestNotifier
   class Report
-    attr_reader :harvest_client
+    attr_reader :harvest_client, :emails_whitelist, :missing_hours_threshold
 
     delegate :users_list, :time_report_list, to: :harvest_client, prefix: :harvest
 
     def initialize(harvest_client)
       @harvest_client = harvest_client
+
+      @emails_whitelist = ENV.fetch("EMAILS_WHITELIST", "").split(",").map(&:strip)
+      @missing_hours_threshold = ENV.fetch("MISSING_HOURS_THRESHOLD", 1.0).to_f
     end
 
     def daily
-      reports = prepare_reports(harvest_time_report_list(Date.yesterday))
+      users = prepare_users(harvest_users_list)
+      reports = harvest_time_report_list(Date.yesterday)
 
-      users.reject do |user|
-        whitelisted_user?(user) || time_reported?(reports, user)
+      filter_users_with_reports(users, reports) do |user|
+        whitelisted_user?(user) || time_reported?(user)
       end
     end
 
-    def weekly; end
+    def weekly
+      users = prepare_users(harvest_users_list)
+      reports = harvest_time_report_list(Date.today.last_week, Date.today.last_week + 4)
+
+      filter_users_with_reports(users, reports) do |user|
+        whitelisted_user?(user) || full_time_reported?(user)
+      end
+    end
 
     private
 
-    def emails_whitelist
-      @emails_whitelist ||= ENV["EMAILS_WHITELIST"].split(",").map(&:strip)
+    def prepare_users(users)
+      users["users"]
+        .group_by { |u| u["id"] }
+        .transform_values { |u| prepared_user(u.first) }
+    end
+
+    def prepared_user(user)
+      hours = user["weekly_capacity"] / 3600
+
+      {
+        "email" => user["email"],
+        "weekly_capacity" => hours,
+        "missing_hours" => hours,
+        "total_hours" => 0
+      }
+    end
+
+    def filter_users_with_reports(users, reports)
+      prepare_users_with_reports(users, reports)
+        .reject { |_id, user| yield(user) }
+        .values
+    end
+
+    def prepare_users_with_reports(users, reports)
+      reports["results"].each.with_object(users) do |report, result|
+        id = report["user_id"]
+
+        result[id]["missing_hours"] -= report["total_hours"]
+        result[id]["total_hours"] += report["total_hours"]
+      end
     end
 
     def whitelisted_user?(user)
       emails_whitelist.include?(user["email"])
     end
 
-    def prepare_reports(reports)
-      reports["results"].map { |r| r["user_id"] }
+    def time_reported?(user)
+      user["total_hours"].positive?
     end
 
-    def time_reported?(reports, user)
-      reports.include?(user["id"])
+    def full_time_reported?(user)
+      time_reported?(user) && missing_hours_insignificant?(user)
     end
 
-    def users
-      @users ||= harvest_users_list["users"].map { |u| u.slice("id", "email") }
+    def missing_hours_insignificant?(user)
+      user["missing_hours"] <= missing_hours_threshold
     end
   end
 end
--- spec/harvest_notifier/report_spec.rb
+++ spec/harvest_notifier/report_spec.rb
@@ -5,53 +5,88 @@ describe HarvestNotifier::Report do
 
   let(:harvest) { instance_double(HarvestNotifier::Harvest) }
 
+  let(:john_doe) do
+    {
+      "id" => 123,
+      "email" => "john.smith@example.com",
+      "weekly_capacity" => 144_000
+    }
+  end
+
+  let(:bill_doe) do
+    {
+      "id" => 345,
+      "email" => "bill.doe@example.com",
+      "weekly_capacity" => 144_000
+    }
+  end
+
+  let(:harvest_users) do
+    {
+      "users" => [john_doe, bill_doe]
+    }
+  end
+
   before do
     allow(harvest).to receive(:users_list) { harvest_users }
-    allow(harvest).to receive(:time_report_list).with(from) { harvest_daily_time_report }
+    allow(harvest).to receive(:time_report_list) { harvest_time_report }
   end
 
   describe "#daily" do
-    let(:from) { Date.new(2020, 4, 15) }
-
-    let(:harvest_users) do
-      {
-        "users" => [
-          {
-            "id" => 123,
-            "email" => "john.smith@example.com"
-          },
-          {
-            "id" => 345,
-            "email" => "bill.doe@example.com"
-          }
-        ]
-      }
-    end
-
-    let(:harvest_daily_time_report) do
+    let(:harvest_time_report) do
       {
         "results" => [
           {
-            "user_id" => 123,
+            "user_id" => john_doe["id"],
             "total_hours" => 6.0
           }
         ]
       }
     end
 
-    let(:expected_resutls) do
-      [
-        {
-          "id" => 345,
-          "email" => "bill.doe@example.com"
-        }
-      ]
+    around do |ex|
+      Timecop.freeze(Time.local(2020, 4, 16)) { ex.run }
     end
 
-    it "returns daily report data" do
-      Timecop.freeze(Time.local(2020, 4, 16)) do
-        expect(report.daily).to eq(expected_resutls)
-      end
+    it "returns Bill Doe without time reports" do
+      expect(report.daily).to include(include("email" => bill_doe["email"]))
+    end
+
+    it "does not return John Doe with time report" do
+      expect(report.daily).not_to include(include("email" => john_doe["email"]))
+    end
+  end
+
+  describe "#weekly" do
+    let(:harvest_time_report) do
+      {
+        "results" => [
+          {
+            "user_id" => john_doe["id"],
+            "total_hours" => 39.0
+          },
+          {
+            "user_id" => bill_doe["id"],
+            "total_hours" => 35.0
+          }
+        ]
+      }
+    end
+
+    around do |ex|
+      Timecop.freeze(Time.local(2020, 4, 20)) { ex.run }
+    end
+
+    it "returns array of users" do
+      expect(report.weekly).to be_a_kind_of(Array)
+    end
+
+    it "returns Bill Does with missing 5 hours" do
+      expect(report.weekly).to include(include("email" => bill_doe["email"], "missing_hours" => 5.0))
+    end
+
+    it "does not return John Doe with missing 1 hour b/c of threshold default 1.0 hour" do
+      expect(report.weekly).not_to include(include("email" => john_doe["email"]))
     end
   end
 end

15bc12a13361f244b2d90aadcb073af7d292d5eb
Timur Vafin <timur.vafin@flatstack.com>
2020-05-08 16:07:48 +0300

    Add Slack mention to template

--- spec/harvest_notifier/templates/daily_report_spec.rb
+++ spec/harvest_notifier/templates/daily_report_spec.rb
@@ -4,11 +4,11 @@ describe HarvestNotifier::Templates::DailyReport do
   subject(:template) { described_class.generate(users: users, date: Date.yesterday) }
 
   describe "#generate" do
-    let(:users) { [{ email: "bill.doe@example.com", id: "U02TEST" }] }
+    let(:users) { [{ email: "bill.doe@example.com", slack_id: "U02TEST" }] }
 
     it "generates template with mentioning users" do
       expect(template).to include("Here is a list of people")
-      expect(template).to include("bill.doe@example.com")
+      expect(template).to include("<@U02TEST>")
     end
   end
 end

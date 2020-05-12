23b383633d42948b5ad81b7e16b5847edc87b8d7
Timur Vafin <timur.vafin@flatstack.com>
2020-05-13 02:24:53 +0300

    Move dates to HarvestNotifier from Base (#18)

--- spec/harvest_notifier_spec.rb
+++ spec/harvest_notifier_spec.rb
@@ -12,15 +12,33 @@ describe HarvestNotifier do
 
   describe "#create_daily_report" do
     it "creates daily report" do
-      expect(base_bouble).to receive(:create_daily_report)
-      described_class.create_daily_report
+      Timecop.freeze(Date.new(2020, 4, 16)) do
+        expect(base_bouble).to receive(:create_daily_report)
+        described_class.create_daily_report
+      end
+    end
+
+    it "does not create daily report on Sat (April 18, 2020)" do
+      Timecop.freeze(Date.new(2020, 4, 18)) do
+        expect(base_bouble).not_to receive(:create_daily_report)
+        described_class.create_daily_report
+      end
     end
   end
 
   describe "#create_weekly_report" do
-    it "creates daily report" do
-      expect(base_bouble).to receive(:create_weekly_report)
-      described_class.create_weekly_report
+    it "creates weekly report" do
+      Timecop.freeze(Date.new(2020, 4, 13)) do
+        expect(base_bouble).to receive(:create_weekly_report)
+        described_class.create_weekly_report
+      end
+    end
+
+    it "does not create weekly report workday except Monday" do
+      Timecop.freeze(Date.new(2020, 4, 14)) do
+        expect(base_bouble).not_to receive(:create_weekly_report)
+        described_class.create_weekly_report
+      end
     end
   end
 end

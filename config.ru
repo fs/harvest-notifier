0314aa9bc3081c49e08298ba5993215f6e2fe2d7
Timur Vafin <timur.vafin@flatstack.com>
2020-05-13 04:35:11 +0300

    Debug

--- config.ru
+++ config.ru
@@ -3,6 +3,7 @@
 lib_dir = File.expand_path("lib", File.dirname(__FILE__))
 $LOAD_PATH << lib_dir unless $LOAD_PATH.include?(lib_dir)
 
+require "harvest_notifier"
 require "harvest-notifier/slack_handler"
 
 run HarvestNotifier::SlackHandler.new
--- lib/harvest-notifier/slack_handler.rb
+++ lib/harvest-notifier/slack_handler.rb
@@ -6,8 +6,6 @@ require "active_support/json"
 require "active_support/core_ext/object/json"
 require "active_support/core_ext/string/conversions"
 
-require "harvest-notifier/base"
-
 module HarvestNotifier
   class SlackHandler
     ACTION_TYPES = %w[daily weekly].freeze

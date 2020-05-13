eced4abf948cf32ec8e736e95b03e2de9c5dc63a
Timur Vafin <timur.vafin@flatstack.com>
2020-05-13 13:32:33 +0300

    Add env to debug

--- lib/harvest_notifier/slack_handler.rb
+++ lib/harvest_notifier/slack_handler.rb
@@ -33,6 +33,7 @@ module HarvestNotifier
     end
 
     def unprocessable_entity(message)
+      puts "Env: #{@env}"
       puts "Params: #{request.params}"
       puts "Payload: #{payload_json}"
       puts "Error: #{message}"
